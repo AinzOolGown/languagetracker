21c6c49b43620a1908e6b2bca39076c254f2d252
Bruno Rico <ricobruno58@gmail.com>
2026-03-14 12:34:21 -0400

    setting up the database helper

---
+++ duoproject/lib/database/database_helper.dart
@@ -0,0 +1,11 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+
+  static final DatabaseHelper instance = DatabaseHelper._init();
+
+  static Database? _database;
+
+  DatabaseHelper._init();
+}
